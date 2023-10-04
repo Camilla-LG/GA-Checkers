89f38c1e34c13402d9f190ce497eb9e9809876d0
camillalo1 <camillalo@getacademy.no>
2023-10-04 16:54:20 +0200

    init

---
+++ script.js
@@ -0,0 +1,44 @@
+// const model = {
+//     app:{
+//         checkersPage,
+//     },
+//     input:{
+
+//     },
+//     data:{
+//         board:{
+//             whiteTiles:"",
+//             blackTiles:"",
+//         },
+//         pieces:{
+//             black:"",
+//             white:"",
+//         },
+//     },
+// }
+
+
+updateView();
+function updateView(){
+    document.getElementById("checkers").innerHTML= /*HTML*/`
+    
+    <h1> Checkers </h1>
+
+    <div id="checkersboard"></div>
+  `;
+  checkersboard();
+}
+
+function checkersboard(){
+    let board = document.getElementById('checkersboard');
+    for (let i = 0; i < 8; i++){
+        for (let j = 0; j < 8; j++){
+            let checkersSquare = document.createElement('div');
+            checkersSquare.className =  'checkers-square';
+            if ((i + j) % 2 == 0){
+                checkersSquare.style.backgroundColor = '#000';
+            }
+            board.appendChild(checkersSquare);
+        }
+    }
+}
